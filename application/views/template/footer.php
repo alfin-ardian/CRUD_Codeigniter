1b834adcd91b16b4480b5612249beda33481614a
alfin ardian <alfin.ardian2020@gmail.com>
2020-09-07 10:51:04 +0700

    menambah fitur crud di mvc

---
+++ application/views/template/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div>
+        <hr />
+        <span>belajar crud codeigniter <?= date('Y'); ?></span>
+    </div>
+
+</footer>
+</body>
+
+</html>
---
+++ application/views/template/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>starter template bootstrap</title>
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap-datepicker3.min.css" rel="stylesheet">
+    <script src="<?php echo base_url(); ?>assets/js/jquery-3.5.0.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap-datepicker.min.js"></script>
+</head>
+
+<body>
